c195eddebec9d69b81d3b33e4d98d66818d57b2a
John Vandenberg <jayvdb@gmail.com>
2019-05-30 13:57:05 +0700

    find_command: Use OpenKey

--- sarge/utils.py
+++ sarge/utils.py
@@ -103,7 +103,8 @@ if sys.platform == 'win32':
             try:
                 ftype = winreg.QueryValue(HKCR, extn)
                 path = os.path.join(ftype, 'shell', 'open', 'command')
-                s = winreg.QueryValue(HKCR, path)
+                key = winreg.OpenKey(HKCR, path)
+                s, _ = winreg.QueryValueEx(key, None)
                 exe = None
                 m = COMMAND_RE.match(s)
                 if m:
